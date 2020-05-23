0a620d8079ed7ae558e9c876361c5d988a245b3f
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2020-05-24 00:38:20 +0200

    v0.1.0

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp -rvu ~/.config/bspwm ./.config/
+cp -rvu ~/.config/conky ./.config/
+cp -rvu ~/.config/kitty ./.config/
+cp -rvu ~/.config/sxhkd ./.config/
+cp -rvu ~/.config/starship.toml ./.config/starship.toml
+cp -vu ~/.vimrc ./.vimrc
+cp -vu ~/.xinitrc ./.xinitrc
